75491aaef1f655fa8d492bf5cb6e8bab25792ca9
Constanza Gonzalez <constanzagonzalez71@gmail.com>
2024-11-03 15:55:15 -0300

    primer

---
+++ JS/scrip.js
@@ -0,0 +1,7 @@
+// Ejercicio 1: Solicitar número y verificar si es mayor a 1000
+let numero = parseInt(prompt("Ingrese un número:"));
+if (numero > 1000) {
+    alert("El número ingresado es mayor a 1000.");
+} else {
+    console.log("El número es menor o igual a 1000.");
+}
